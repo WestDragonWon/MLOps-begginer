57974ba5c8a170e322e856b9d939e55776f937c8
westdragonwon <westdragonwon@gmail.com>
2024-08-12 02:17:35 +0000

    모델 파일 검증 추가하기

--- mlops-movie-predictor/src/utils/utils.py
+++ mlops-movie-predictor/src/utils/utils.py
@@ -1,5 +1,6 @@
 import os
 import random
+import hashlib
 
 import numpy as np
 import torch
@@ -28,3 +29,23 @@ def auto_increment_run_suffix(name: str, pad=3):
     suffix = name.split("-")[-1]
     next_suffix = str(int(suffix) + 1).zfill(pad)
     return name.replace(suffix, next_suffix)
+
+def calculate_hash(filename):
+    sha256_hash = hashlib.sha256()
+    with open(filename, "rb") as f:
+        for byte_block in iter(lambda: f.read(4096), b""):
+            sha256_hash.update(byte_block)
+    return sha256_hash.hexdigest()
+
+def save_hash(dst):
+    hash_ = calculate_hash(dst)
+    dst, _ = os.path.splitext(dst)
+    with open(f"{dst}.sha256", "w") as f:
+        f.write(hash_)
+
+
+def read_hash(dst):
+    dst, _ = os.path.splitext(dst)
+    with open(f"{dst}.sha256", "r") as f:
+        return f.read()
+    
